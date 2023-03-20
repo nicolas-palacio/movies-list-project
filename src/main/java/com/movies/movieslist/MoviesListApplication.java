2ae5f93fa300d9ab260b005b0181426ca019684b
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-03-19 21:40:54 -0300

    Initial commit

---
+++ src/main/java/com/movies/movieslist/MoviesListApplication.java
@@ -0,0 +1,13 @@
+package com.movies.movieslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/movieslist/MoviesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.movieslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
