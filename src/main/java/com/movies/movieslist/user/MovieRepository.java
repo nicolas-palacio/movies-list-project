a34a4f6764abd2b610ed51c753bc3a55e2e54ef9
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-03-24 13:57:44 -0300

    Add Movie repo interface

---
+++ src/main/java/com/movies/movieslist/user/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.movies.movieslist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+}
