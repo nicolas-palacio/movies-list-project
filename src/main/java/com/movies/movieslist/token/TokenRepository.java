ed6924542ba692399345b8ba591de0a7d888c4a7
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-03-25 02:04:26 -0300

    Fix token repository query

--- src/main/java/com/movies/movieslist/token/TokenRepository.java
+++ src/main/java/com/movies/movieslist/token/TokenRepository.java
@@ -8,9 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TokenRepository  extends JpaRepository<Long,Token> {
-    @Query(value=" select t from Token t inner join User u on t.user.id=u.id where u.id=:id and " +
-            "(t.expired=false or t.revoked=false)")
+public interface TokenRepository  extends JpaRepository<Token,Long> {
+    @Query(value = """
+      select t from Token t inner join User u\s
+      on t.user.id = u.id\s
+      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      """)
     List<Token>findAllValidTokenByUser(Long id);
 
     Optional<Token> findByToken(String token);
