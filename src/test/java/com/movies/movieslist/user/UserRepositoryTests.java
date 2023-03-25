06e4bced0ee801e3f43068c062848363c6817484
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-03-25 13:13:35 -0300

    Add H2 database dependency

--- src/test/java/com/movies/movieslist/user/UserRepositoryTests.java
+++ src/test/java/com/movies/movieslist/user/UserRepositoryTests.java
@@ -1,7 +1,13 @@
 package com.movies.movieslist.user;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class UserRepositoryTests {
+
+    @Test
+    void saveUserTest(){
+
+    }
 }
