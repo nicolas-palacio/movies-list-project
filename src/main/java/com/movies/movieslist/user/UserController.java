0be4aaa61ad711ca573e2fb78ab831c4ede41e00
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-04-02 20:10:19 -0300

    Add swagger anotations on the user controller

--- src/main/java/com/movies/movieslist/user/UserController.java
+++ src/main/java/com/movies/movieslist/user/UserController.java
@@ -1,6 +1,7 @@
 package com.movies.movieslist.user;
 
 import com.movies.movieslist.user.util.UserInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -17,21 +18,28 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Return the info of the user.",tags = {"User"})
     @GetMapping("/info")
+    @ResponseBody
     public ResponseEntity<Object> getUserInfo(){
         UserInfoResponse userInfo=userService.getUserInfo();
 
         return  new ResponseEntity<>(userInfo,HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Return the list of movies of the user.",tags = {"User"})
     @GetMapping("/list")
+    @ResponseBody
     public ResponseEntity<List<Movie>> getUserList(){
         List<Movie> userMovies=userService.getUserMovies();
 
         return  new ResponseEntity<>(userMovies,HttpStatus.OK);
     }
 
-    @PostMapping("/addMovie")
+    @Operation(summary = "Post a movie on the list of the user.",tags = {"User"})
+    @PostMapping("/add-movie")
+    @ResponseBody
     public ResponseEntity<Object> addMovieToUser(@RequestBody Movie movie){
         Movie movieToAdd=null;
         if(SecurityContextHolder.getContext().getAuthentication()!=null){
