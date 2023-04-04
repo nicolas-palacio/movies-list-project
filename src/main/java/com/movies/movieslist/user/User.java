a94344f7774180aafae26f5bcd0fede7ddadc193
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-04-04 19:29:36 -0300

    Add username attribute

--- src/main/java/com/movies/movieslist/user/User.java
+++ src/main/java/com/movies/movieslist/user/User.java
@@ -22,6 +22,8 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String username;
     private String firstname;
     private String lastname;
     private String email;
