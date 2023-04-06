c620687b7f0bf746ca4274559d1a1dd8a4c9a9c4
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-04-06 01:26:09 -0300

    Add register request annotations

--- src/main/java/com/movies/movieslist/auth/RegisterRequest.java
+++ src/main/java/com/movies/movieslist/auth/RegisterRequest.java
@@ -5,18 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotNull(message = "The username must be not null")
     private String username;
     //private String firstname;
     //private String lastname;
+    @NotNull(message = "The email must be not null")
     private String email;
+
+    @NotNull(message = "The country must be not null")
     private String country;
+
+    @NotNull(message = "The password must be not null")
     private String password;
+    @NotNull(message = "You must to confirm the password")
 
     private String passwordConfirm;
 }
--- src/main/java/com/movies/movieslist/movie/Movie.java
+++ src/main/java/com/movies/movieslist/movie/Movie.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 @Table(name = "movie")
 public class Movie {
     @Id
+    @Column(unique = true)
     private Long id;
     @NotNull
     private String name;
