05e153b228bf2588b7af39dd3599f8e0b01a2738
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-04-09 03:33:42 -0300

    Added user service tests class

---
+++ src/test/java/com/movies/movieslist/user/UserServiceTests.java
@@ -0,0 +1,12 @@
+package com.movies.movieslist.user;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+    @Mock
+    private UserRepository userRepository;
+}
